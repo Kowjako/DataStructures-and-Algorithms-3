0aaa96ef6c0f713993f87267c9f5a6ffeb1cd889
Uladzimir Kaviaka <kowyako@yandex.ru>
2021-10-11 15:18:00 +0200

    Worked reading graph from file and printing

--- PEA1/bruteforce.cpp
+++ PEA1/bruteforce.cpp
@@ -7,5 +7,56 @@ BruteForce::BruteForce()
 
 BruteForce::~BruteForce()
 {
-   //dtor
+   for(int i=0;i<this->node_num ;i++)
+        delete[] this->macierz[i];
+       delete[] this->macierz;
+}
+
+void BruteForce::SetValueForMatrix(int i, int j ,int value)
+{
+   this->macierz[i][j] = value;
+}
+
+bool BruteForce::ReadFromFile(string filename)
+{
+   ifstream file(filename);
+   if(file.is_open())
+   {
+      int distance;
+      file>>this->node_num;                           /* inicjalizacja liczby wierzcholkow */
+
+      if(file.fail())
+      {
+         cout<<"Blad podczas odczytywania poczatkowych wartosci"<<endl;
+         return false;
+      }
+
+      this->macierz = new int*[this->node_num];    /* alokacja macierzy sasiedztwa */
+
+      for(int i = 0;i<this->node_num;i++)           /* uzupelnianie macierzy sasiedztwa */
+      {
+         this->macierz[i] = new int[this->node_num];
+         for(int j = 0;j<this->node_num;j++)
+         {
+            file >> distance;
+            SetValueForMatrix(i,j, distance);
+         }
+      }
+      return true;
+
+   }
+   else
+   {
+      cout<<"Blad podczas otwierania pliku"<<endl;
+      return false;
+   }
+}
+
+void BruteForce::PrintGraph()
+{
+   for(int i=0;i<node_num;i++) {
+        for(int j = 0;j<node_num;j++)
+            cout<<this->macierz[i][j]<<'\t';
+        cout<<endl;
+   }
 }
--- PEA1/bruteforce.h
+++ PEA1/bruteforce.h
@@ -1,6 +1,15 @@
 #ifndef BRUTEFORCE_H
 #define BRUTEFORCE_H
+#include <iostream>
+#include <fstream>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <stdlib.h>
+#include <time.h>
+#include "node.h"
 
+using namespace std;
 
 class BruteForce
 {
@@ -9,9 +18,12 @@ class BruteForce
       virtual ~BruteForce();
       bool ReadFromFile(string filename);
 
+      void PrintGraph();
+
    protected:
 
    private:
+      int node_num = 0;
       int** macierz;  /* macierz sasiedztwa */
       void SetValueForMatrix(int i, int j, int value);
 };
