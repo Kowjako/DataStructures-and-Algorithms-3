b6055e08ac8c8d8708b654426fc81044f3d59f30
Włodzimierz <Kowjako>
2021-10-14 08:17:15 +0200

    Worked reading from file and printing for graf

--- PEA1/dynamicprogramming.cpp
+++ PEA1/dynamicprogramming.cpp
@@ -9,3 +9,47 @@ DynamicProgramming::~DynamicProgramming()
 {
    //dtor
 }
+
+bool DynamicProgramming::ReadFromFile()
+{
+   ifstream file(filename);
+   if(file.is_open())
+   {
+      int distance;
+      file>>this->node_num;                           /* inicjalizacja liczby wierzcholkow */
+
+      if(file.fail())
+      {
+         cout<<"Blad podczas odczytywania poczatkowych wartosci"<<endl;
+         return false;
+      }
+
+      this->macierz = new int*[this->node_num];    /* alokacja macierzy sasiedztwa */
+
+      for(int i = 0;i<this->node_num;i++)           /* uzupelnianie macierzy sasiedztwa */
+      {
+         this->macierz[i] = new int[this->node_num];
+         for(int j = 0;j<this->node_num;j++)
+         {
+            file >> distance;
+            SetValueForMatrix(i,j, distance);
+         }
+      }
+      return true;
+
+   }
+   else
+   {
+      cout<<"Blad podczas otwierania pliku"<<endl;
+      return false;
+   }
+}
+
+void DynamicProgramming::PrintGraph() 
+{
+	for(int i=0;i<node_num;i++) {
+        for(int j = 0;j<node_num;j++)
+            cout<<this->macierz[i][j]<<'\t';
+        cout<<endl;
+   }
+}
