88b25f70e10c49196c7d888c2e9b10f835907e66
Włodzimierz <Kowjako>
2021-10-14 08:15:15 +0200

    Start definition for DP header

--- PEA1/dynamicprogramming.h
+++ PEA1/dynamicprogramming.h
@@ -1,16 +1,34 @@
 #ifndef DYNAMICPROGRAMMING_H
 #define DYNAMICPROGRAMMING_H
 
+#include <iostream>
+#include <fstream>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <stdlib.h>
+#include <time.h>
 
+using namespace std;
 class DynamicProgramming
 {
    public:
       DynamicProgramming();
       virtual ~DynamicProgramming();
-
+	  
+	  void PrintGraph();
+	  void PrintSolution();
+	  
+	  void Start();
+	  
+	  bool ReadFromFile(string filename)
+	  
    protected:
-
+   
    private:
+      int node_num = 0;
+	  int** macierz;
+	  void SetMatrixValue(int i, int j, int value);
 };
 
 #endif // DYNAMICPROGRAMMING_H
