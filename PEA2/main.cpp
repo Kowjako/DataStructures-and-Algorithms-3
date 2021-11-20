d791a50d319821e168104a9ea015f851ab905784
Uladzimir Kaviaka <kowyako@yandex.ru>
2021-11-20 16:52:10 +0100

    Add start version of menu for PEA2

--- PEA2/main.cpp
+++ PEA2/main.cpp
@@ -1,28 +1,76 @@
 #include <iostream>
 #include "graphreader.h"
 #include "tabusearch.h"
+#include "simulatedannealing.h"
+#include <windows.h>
+#include <iomanip>
 
 using namespace std;
 
 GraphReader reader;
 TabuSearch ts;
+SimulatedAnnealing sa;
+
+long long int read_QPC()
+{
+    LARGE_INTEGER count;
+    QueryPerformanceCounter(&count);
+    return((long long int)count.QuadPart);
+}
+
+long long int frequency, start, elapsed;
 
 int main()
 {
+    int operationNumber;
+    double stopTime;
+    double freezingLevel;
     string loc;
-    cout<<"Wprowadz lokalizacje pliku"<<endl;
-    cin>>loc;
+    cout<<"Witam w projekcie PEA 2"<<endl;
 
-    if(reader.ReadFromFile(loc))
+    do
     {
-       cout<<"Pomyslnie wczytano"<<endl;
-       ts.SetMatrix(reader.getMacierz(), reader.getNodeNum());
+        cout<<"1. Wczytaj plik"<<endl;
+        cout<<"2. Wprowadz czas stopu"<<endl;
+        cout<<"3. Tabu Search"<<endl;
+        cout<<"4. Simulated Annealing"<<endl;
+        cout<<"5. Koniec programu"<<endl;
+        cin >> operationNumber;
+        switch(operationNumber)
+        {
+            case 1:
+               cout<<"Wprowadz lokalizacje pliku"<<endl;
+               cin>>loc;
+               if(reader.ReadFromFile(loc))
+               {
+                  cout<<"Pomyslnie wczytano"<<endl;
+                  ts.SetMatrix(reader.getMacierz(), reader.getNodeNum());
+                  sa.SetMatrix(reader.getMacierz(), reader.getNodeNum());
+               }
+               break;
+            case 2:
+               cout<<"Wprowadz czas stopu"<<endl;
+               cin>>stopTime;
+               break;
+            case 3:
+               ts.SetStopTime(stopTime);
+               ts.StartAlgorithm();
+               ts.PrintSolution();
+               break;
+            case 4:
+               sa.SetStopTime(stopTime);
+               cout<<"Wprowadz wspolczynnik chlodzenia, np: 0.95"<<endl;
+               cin>>freezingLevel;
+               srand(time(NULL));
+               sa.SetFreezingLevel(freezingLevel);
+               sa.StartAlgorithm();
+               sa.PrintSolution();
+               break;
+            default:
+               break;
+        }
     }
-
-    ts.SetStopTime(600);
-
-    ts.StartAlgorithm();
-    ts.PrintSolution();
+    while(operationNumber!=5);
 
     return 0;
 }
